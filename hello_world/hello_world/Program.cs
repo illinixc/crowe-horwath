90c7ff5aa880bc1adaa8cb35ef9efa2cbb1cb461
Andrew Kurtz <andrew.kurtz@gmail.com>
2017-04-06 13:11:22 -0400

    removed redundant commit (after moved note to readme)

--- hello_world/hello_world/Program.cs
+++ hello_world/hello_world/Program.cs
@@ -21,18 +21,6 @@ namespace hello_world
 
 		}
 
-		/// <summary>
-		/// Configure the specified services. 
-		/// As I dont currently have a Windows machine, this was my first attempt at using 
-		/// the .Net App Core on a Mac. Unity and Ninject aren't currently compatible. Since using configuration file
-		/// was in the requirements, I would probably have used Unity for the appSettings type binding. I realize writing the
-		/// Assembly Full Name in the appsettings.json is just asking for a runtime error. This implemention allows for
-		/// binding a single writer. If multiple writers is required then it would be a different pattern, such as chain of 
-		/// responsibility with an array of writers, to be configured or turned off/on explicitly. 
-		/// 
-		/// </summary>
-		/// <returns>void</returns>
-		/// <param name="services">Services.</param>
 		public static void Configure(IServiceCollection services)
 		{
 			var configuration = new ConfigurationBuilder()
